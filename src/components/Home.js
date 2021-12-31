adcff576297d100be9bf37de1b7318b70e66187d
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-31 11:58:44 -0500

    Basic Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import {skills} from '../constants/initialValues';
 function Home() {
     return (
-        <div className='home-card'>
-            
-        </div>
+        <section id='home-section'>
+            <div>
+                <h2>Jayson Byrd</h2>
+                <span>Full-Stack Web Developer</span>
+            </div>
+            <div id='skills'>
+                {skills.map(skill => {
+                    return <p className='skill' id={skill}>{skill}</p>
+                })}
+            </div>
+        </section>
     )
 }
 
